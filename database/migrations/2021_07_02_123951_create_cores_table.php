d68c11ed7389f035327a960aa2b19db4f39dd609
moufakkir-zohair <zohair.moufakkir@usmba.ac.ma>
2021-07-02 14:46:08 +0200

    feat:create_cores_table

---
+++ database/migrations/2021_07_02_123951_create_cores_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cores', function (Blueprint $table) {
+            $table->id();
+            $table->string('core_name');
+            $table->string('core_url');
+            $table->string('core_username');
+            $table->string('core_passhash');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cores');
+    }
+}
