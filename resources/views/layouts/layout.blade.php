5d9f939d5f0f1a7237557af877101a66e71ba00b
moufakkir-zohair <zohair.moufakkir@usmba.ac.ma>
2021-07-02 17:19:38 +0200

    fix:change-layout-commun

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -13,10 +13,11 @@
       <div class="container">
         @yield('content')
       </div> 
-
+      
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
       <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
       <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+      @yield('script')
 </body>
 </html>
