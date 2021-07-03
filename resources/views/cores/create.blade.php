9736641ffb09e83bad7be660c40293abe6050fb6
moufakkir-zohair <zohair.moufakkir@usmba.ac.ma>
2021-07-03 12:48:21 +0200

    fix:display-message-error-in-index-core

--- resources/views/cores/create.blade.php
+++ resources/views/cores/create.blade.php
@@ -5,19 +5,39 @@
            @csrf
             <div class="form-group">
                 <label>Name</label>
-                <input type="text" class="form-control" name="core_name" placeholder="Name"/>
+                <input type="text" class="form-control" name="core_name" placeholder="Name" value="{{old('core_name')}}"/>
+                <span style="color: red">
+                    @error('core_name')
+                        {{$message}}
+                    @enderror
+                </span>
             </div>
             <div class="form-group">
               <label>Username</label>
-              <input type="text" class="form-control" name="core_username" placeholder="Username">
+              <input type="text" class="form-control" name="core_username" placeholder="Username"  value="{{old('core_username')}}">
+              <span style="color: red">
+                @error('core_username')
+                    {{$message}}
+                @enderror
+            </span>
             </div>
             <div class="form-group">
               <label>Url</label>
-              <input type="url" class="form-control" name="core_url" placeholder="https://.....">
+              <input type="url" class="form-control" name="core_url" placeholder="https://....."  value="{{old('core_url')}}">
+              <span style="color: red">
+                @error('core_url')
+                    {{$message}}
+                @enderror
+            </span>
             </div>
             <div class="form-group">
               <label>password</label>
-              <input type="text" class="form-control" name="password" placeholder="********">
+              <input type="password" class="form-control" name="password" placeholder="********" value="{{old('password')}}">
+              <span style="color: red">
+                @error('password')
+                    {{$message}}
+                @enderror
+            </span>
             </div>
             <button  class="btn btn-block btn-primary" type="submit"> Add core</button>
     </form>
