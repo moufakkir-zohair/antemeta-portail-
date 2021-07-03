d37dd69f4d7923054c8c1a2f5f56223d92d09218
moufakkir-zohair <zohair.moufakkir@usmba.ac.ma>
2021-07-03 13:54:06 +0200

    fix:add-form-delete-index-core

--- resources/views/cores/index.blade.php
+++ resources/views/cores/index.blade.php
@@ -21,13 +21,34 @@
             <td>{{$core->core_username}}</td>
             <td> 
                     <button class="btn btn-success" data-toggle="modal" data-target="#editmodel" ><i class="fa fa-pencil-square-o" aria-hidden="true"></i>Modifier</button> 
-                    <button class="btn btn-danger" data-toggle="modal" data-target="#deletemodel"  style="padding-left: 4px;padding-right: 4px;"><i class="fa fa-trash" aria-hidden="true" ></i>Supprimer</button>
+                    <button class="btn btn-danger" data-toggle="modal" data-target="#deletemodel" style="padding-left: 4px;padding-right: 4px;"><i class="fa fa-trash" aria-hidden="true" ></i>Supprimer</button>
             </td>
-           
+            <div class="modal fade" id="deletemodel" tabindex="-1" role="dialog" aria-labelledby="delete" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                <form action="{{ route('Cores.destroy',['Core'=>$core->id])}}" method="POST">
+                  @csrf
+                  @method('DELETE')
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Delete core</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
+                  </div>
+                  <div class="modal-body">
+                  <input type="hidden" name="message" >Do you want to remove this core? </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+                    <button type="submit" class="btn btn-danger"> Yes </button>
+                  </div>
+                </div>
+              </form>
+              </div>
+            </div>
         </tr>
         @endforeach
     </tbody>
 </table>
+
+
 @endsection
 
 @section('script')
@@ -35,5 +56,14 @@
         $(document).ready(function() {
             $('#example').DataTable();
         } );
+
+        $('#deletemodel').on('show.bs.modal',function(event){
+        var button = $(event.relatedTarget)
+       // var delete_id=button.data('delete_id')
+        
+        var modal=$(this)
+        //modal.find('.modal-body #delete_id').val(delete_id);
+        
+       })
     </script>
 @endsection
