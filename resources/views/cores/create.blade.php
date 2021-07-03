f738e74f270e9eccf9ccedbce03f175cdf816908
moufakkir-zohair <zohair.moufakkir@usmba.ac.ma>
2021-07-03 11:57:49 +0200

    feat:view create-cores

---
+++ resources/views/cores/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts/layout')
+@section('content')
+<h1>Add core</h1>
+    <form method="POST" action="{{route('Cores.store')}}">
+           @csrf
+            <div class="form-group">
+                <label>Name</label>
+                <input type="text" class="form-control" name="core_name" placeholder="Name" required/>
+            </div>
+            <div class="form-group">
+              <label>Username</label>
+              <input type="text" class="form-control" name="core_username" placeholder="Username" required>
+            </div>
+            <div class="form-group">
+              <label>Url</label>
+              <input type="url" class="form-control" name="core_url" placeholder="https://....." required>
+            </div>
+            <div class="form-group">
+              <label>password</label>
+              <input type="text" class="form-control" name="password" placeholder="********" >
+            </div>
+            <button  class="btn btn-block btn-primary" type="submit"> Add core</button>
+    </form>
+@endsection
