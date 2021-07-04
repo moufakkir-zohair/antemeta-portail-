364995add22ad962e7258cec5bbe46edf27b7a32
moufakkir-zohair <zohair.moufakkir@usmba.ac.ma>
2021-07-05 01:57:20 +0200

    feat:add-edit-action-to-CoreController

--- app/Http/Controllers/CoreController.php
+++ app/Http/Controllers/CoreController.php
@@ -10,33 +10,18 @@ use Illuminate\Support\Facades\Hash;
 
 class CoreController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+  
     public function index()
     {
         $cores  = Core::all();
         return view("cores.index", compact('cores'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view("cores.create");
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(StroreCoreRequest $request)
     {
         Core::create([
@@ -49,46 +34,21 @@ class CoreController extends Controller
         return redirect()->route("cores.index");
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function edit(Core $core)
     {
-        //
+        return view('cores.edit',compact('core'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
