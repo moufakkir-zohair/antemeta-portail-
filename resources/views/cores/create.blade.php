4efe7e7ae4529beb5272fe3fd9339cd62dd0d4ad
moufakkir-zohair <zohair.moufakkir@usmba.ac.ma>
2021-07-05 02:50:46 +0200

    fix:change_name-of-route-in-create-view

--- resources/views/cores/create.blade.php
+++ resources/views/cores/create.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts/layout')
 @section('content')
 <h1>Add core</h1>
-    <form method="POST" action="{{route('Cores.store')}}">
+    <form method="POST" action="{{route('cores.store')}}">
            @csrf
             <div class="form-group">
                 <label>Name</label>
-                <input type="text" class="form-control" name="core_name" placeholder="Name" value="{{old('core_name')}}"/>
+                <input type="text" name="core_name"  class="form-control @error('core_name') is-invalid  @enderror" placeholder="Name" value="{{old('core_name')}}"/>
                 <span style="color: red">
                     @error('core_name')
                         {{$message}}
@@ -14,7 +14,7 @@
             </div>
             <div class="form-group">
               <label>Username</label>
-              <input type="text" class="form-control" name="core_username" placeholder="Username"  value="{{old('core_username')}}">
+              <input type="text" class="form-control @error('core_username') is-invalid  @enderror " name="core_username" placeholder="Username"  value="{{old('core_username')}}">
               <span style="color: red">
                 @error('core_username')
                     {{$message}}
@@ -23,7 +23,7 @@
             </div>
             <div class="form-group">
               <label>Url</label>
-              <input type="url" class="form-control" name="core_url" placeholder="https://....."  value="{{old('core_url')}}">
+              <input type="url" class="form-control @error('core_url') is-invalid  @enderror " name="core_url" placeholder="https://....."  value="{{old('core_url')}}">
               <span style="color: red">
                 @error('core_url')
                     {{$message}}
@@ -32,9 +32,9 @@
             </div>
             <div class="form-group">
               <label>password</label>
-              <input type="password" class="form-control" name="password" placeholder="********" value="{{old('password')}}">
+              <input type="password" class="form-control @error('core_passhash') is-invalid  @enderror " name="core_passhash" placeholder="********" value="{{old('core_passhash')}}">
               <span style="color: red">
-                @error('password')
+                @error('core_passhash')
                     {{$message}}
                 @enderror
             </span>
