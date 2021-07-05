366f16384a49dddd10ead47a998d7fe61df14984
moufakkir-zohair <zohair.moufakkir@usmba.ac.ma>
2021-07-05 02:37:07 +0200

    fix:change-view_index

--- resources/views/cores/index.blade.php
+++ resources/views/cores/index.blade.php
@@ -1,69 +1,122 @@
 @extends('layouts/layout')
 @section('content')
-
-    <a href="{{route('Cores.create')}}" class="btn btn-primary" style="margin-top: 2%; margin-bottom:2%;">Add core <i class="fa fa-plus-square-o" aria-hidden="true"></i></a>
-
-    <table id="example" class="display" style="width:100%">
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Url</th>
-            <th>Username</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($cores as $core)
-        <tr>
-           
-            <td>{{$core->core_name}}</td>
-            <td>{{$core->core_url}}</td>
-            <td>{{$core->core_username}}</td>
-            <td> 
-                    <button class="btn btn-success" data-toggle="modal" data-target="#editmodel" ><i class="fa fa-pencil-square-o" aria-hidden="true"></i>Modifier</button> 
-                    <button class="btn btn-danger" data-toggle="modal" data-target="#deletemodel" style="padding-left: 4px;padding-right: 4px;"><i class="fa fa-trash" aria-hidden="true" ></i>Supprimer</button>
-            </td>
-            <div class="modal fade" id="deletemodel" tabindex="-1" role="dialog" aria-labelledby="delete" aria-hidden="true">
-              <div class="modal-dialog" role="document">
-                <form action="{{ route('Cores.destroy',['Core'=>$core->id])}}" method="POST">
-                  @csrf
-                  @method('DELETE')
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Delete core</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
+      <div class="row">
+          <div class="col-md-12 main-datatable">
+              <div class="card_body">
+                  <div class="row d-flex">
+                      <div class="col-sm-4 createSegment"> 
+                       <a class="btn dim_button create_new" href="{{route('cores.create')}}"> <span><i class="fa fa-plus-square-o" aria-hidden="true"></i></span> Create New</a>
+                      </div>
+                      <div class="col-sm-8 add_flex">
+                          <div class="form-group searchInput">
+                              <label for="search">Search:</label>
+                              <input type="search" class="form-control" id="filterbox" placeholder=" ">
+                          </div>
+                      </div> 
                   </div>
-                  <div class="modal-body">
-                  <input type="hidden" name="message" >Do you want to remove this core? </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
-                    <button type="submit" class="btn btn-danger"> Yes </button>
+                  <div class="overflow-x">
+                      <table style="width:100%;" id="filtertable" class="table cust-datatable dataTable no-footer">
+                          <thead>
+                              <tr>
+                                  <th style="min-width:150px;">Name</th>
+                                  <th style="min-width:150px;">Url</th>
+                                  <th style="min-width:100px;">Username</th>
+                                  <th style="min-width:100px;">Date</th>
+                                  <th style="min-width:150px;">Action</th>
+                              </tr>
+                          </thead>
+                          <tbody>
+
+                            @foreach ($cores as $core)
+                            <tr>
+                                <td>{{$core->core_name}}</td>
+                                <td>{{$core->core_url}}</td>
+                                <td>{{$core->core_username}}</td>
+                                <td>{{$core->created_at->format('d/m/Y')}}</td>
+                                <td> 
+
+                                  <span class="actionCust">
+                                    <a href="{{route('cores.edit',['core'=>$core])}}"><i class="fa fa-pencil-square-o"></i></a>
+                                  </span>
+                                  <span class="actionCust">
+                                    <a href="" data-toggle="modal" data-target="#deletemodel" ><i class="fa  fa-trash"></i></a>
+                                  </span>
+                                  <span class="actionCust">
+                                    <a href="" data-toggle="modal" data-target="#showmodel"><i class="fa  fa-eye"></i></a>
+                                  </span>
+                                </td>
+
+                                <div class="modal fade" id="deletemodel" tabindex="-1" role="dialog" aria-labelledby="delete" aria-hidden="true">
+                                  <div class="modal-dialog" role="document">
+                                    <form action="{{ route('cores.destroy',['core'=>$core])}}" method="POST">
+                                      @csrf
+                                      @method('DELETE')
+                                    <div class="modal-content">
+                                      <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Delete core</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
+                                      </div>
+                                      <div class="modal-body">
+                                      <input type="hidden" name="message" >Do you want to remove this core? </div>
+                                      <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+                                        <button type="submit" class="btn btn-danger"> Yes </button>
+                                      </div>
+                                    </div>
+                                  </form>
+                                  </div>
+                                </div>
+
+                                <div class="modal fade" id="showmodel" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="show" aria-hidden="true">
+                                  <div class="modal-dialog">
+                                    <div class="modal-content">
+                                      <div class="modal-header">
+                                        <h5 class="modal-title">Detail of core</h5>
+                                        <button aria-hidden="true" data-dismiss="modal" class="close"
+                                          type="button">×</button>
+                                      </div>
+                                      <div class="modal-body">
+                                        <h5>core name : {{$core->core_name}}</h5> 
+                                        <h5> username : {{$core->core_username}}</h5> 
+                                        <h5> url : {{$core->core_url}}</h5> 
+                                        <h5> date : {{$core->created_at->format('d/m/Y')}}</h5> 
+                                      </div>
+                                      <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Ok</button>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div> 
+
+                            </tr>
+                          @endforeach
+                          </tbody>
+                      </table>
                   </div>
-                </div>
-              </form>
               </div>
-            </div>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
-
-
+          </div>
+      </div>
 @endsection
 
 @section('script')
-    <script>
-        $(document).ready(function() {
-            $('#example').DataTable();
-        } );
+<script>
+       $(document).ready(function() {
+        var dataTable = $('#filtertable').DataTable({
+          "pageLength":5,
+          'aoColumnDefs':[{
+              'aTargets':['nosort'],
+          }],
+          "bLengthChange":false,
+          "dom":'<"top">ct<"top"p><"clear">'
+        });
+        $("#filterbox").keyup(function(){
+          dataTable.search(this.value).draw();
+        });
+      });
 
         $('#deletemodel').on('show.bs.modal',function(event){
-        var button = $(event.relatedTarget)
-       // var delete_id=button.data('delete_id')
-        
-        var modal=$(this)
-        //modal.find('.modal-body #delete_id').val(delete_id);
-        
-       })
-    </script>
+            var button = $(event.relatedTarget)
+            var modal=$(this)
+        })
+</script>
 @endsection
