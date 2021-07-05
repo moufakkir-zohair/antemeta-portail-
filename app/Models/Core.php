e31acab46aaa8573f1359190744c5b276409a947
moufakkir-zohair <zohair.moufakkir@usmba.ac.ma>
2021-07-05 02:41:45 +0200

    feat:configure-softDeletes-in-Core

--- app/Models/Core.php
+++ app/Models/Core.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Core extends Model
 {
+    use SoftDeletes;
     protected $fillable = ['core_name','core_url','core_username','core_passhash'];
 }
