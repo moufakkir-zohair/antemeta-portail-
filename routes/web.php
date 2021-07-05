2251d8d739d1e679ac2bd592f31a0e389995c0d8
moufakkir-zohair <zohair.moufakkir@usmba.ac.ma>
2021-07-05 02:39:16 +0200

    fix:change-name-route

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CoreController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::resource('Cores',CoreController::class)->except('show');
 
 Route::get('/', [CoreController::class, 'index']);
+Route::get('cores/{core}/restore',[CoreController::class, 'restore'])->name("cores.restore");
+Route::resource('cores',CoreController::class);
