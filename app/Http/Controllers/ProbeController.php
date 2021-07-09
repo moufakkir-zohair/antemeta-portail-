0402001bac542df98818fcc2bc8e291e60d5211d
moufakkir-zohair <zohair.moufakkir@usmba.ac.ma>
2021-07-09 03:01:17 +0200

    add-ProbeController

---
+++ app/Http/Controllers/ProbeController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Probe;
+use App\Models\Core;
+use App\Models\HistoryProbe;
+use Exception;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class ProbeController extends Controller
+{
+    public function CreateProbes($probesAPI = [] , Core $core=null){
+        foreach($probesAPI as $probe){
+            Probe::create([
+            'core_id' => $core->id,
+            'objid' => $probe['objid'],
+            'name' => $probe['name'],
+            'status' => $probe['condition'],
+            'changed' => true
+            ]);
+        }
+    }
+
+    public function UpdateProbes($probesAPI = [] , Core $core=null){
+        foreach($probesAPI as $probe){
+            $probeByid = Probe::where('core_id', $core->id)->where('objid', $probe['objid'])->first();
+            if($probeByid){
+                   $probeByid->update([
+                       'core_id' => $core->id,
+                       'objid' => $probe['objid'],
+                       'name' => $probe['name'],
+                       'status' => $probe['condition'],
+                       'changed' => true,
+                   ]);
+            }else{
+               Probe::create([
+                   'core_id' => $core->id,
+                   'objid' => $probe['objid'],
+                   'name' => $probe['name'],
+                   'status' => $probe['condition'],
+                   'changed' => true,
+                   ]); 
+            }
+       }
+    }
+
+
+    public function ReinitialiserProbes(){
+        $probesDB = Probe::all(); 
+        foreach($probesDB as $probe){
+            echo $probe->changed;
+            if($probe->changed==1){
+                $probe->update([
+                    'changed' => false,
+                ]);
+            }else{
+                $probe->update([
+                    'status' => 'Disabled',
+                    'changed' => false,
+                ]);
+            }
+        }
+    }
+
+    public function AddHistoryProbes($probesDB=[]){
+        foreach($probesDB as $probe){
+            HistoryProbe::create([
+            'core_id' => $probe->core_id,
+            'objid' => $probe->objid,
+            'name' => $probe->name,
+            'status' => $probe->status,
+            ]);
+        }
+    }
+
+    public function index()
+    {   
+        $cores = Core::all();
+        if($cores->count()){
+            $probesDB = Probe::all();
+            $this->AddHistoryProbes($probesDB);
+            foreach($cores as $core ){
+                $probesAPI = Http::get($core->core_url.'api/table.json?columns=objid,name,condition&filter_type=probenode&username='.$core->core_username.'&passhash='.$core->core_passhash)->json()[""];
+                if($probesDB->count()){
+                    $this->UpdateProbes($probesAPI,$core);
+                }else{
+                    $this->CreateProbes($probesAPI,$core);
+                }
+            }
+            $this->ReinitialiserProbes();
+        }   
+    }
+
+
+
+    public function Core_History (){
+        $cores = Core::all();
+        foreach($cores as $core ){
+            $probes = file_get_contents($core->core_url.'api/gettreenodestats.xml?username='.$core->core_username.'&passhash='.$core->core_passhash);
+            $xml = simplexml_load_string($probes, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $json = json_encode($xml);
+            $array = json_decode($json,TRUE);
+            echo $json;
+        }
+    }
+}
