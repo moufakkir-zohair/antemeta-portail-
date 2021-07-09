3f48138e8113e059da28087397c73ff3bef27ca4
moufakkir-zohair <zohair.moufakkir@usmba.ac.ma>
2021-07-09 21:43:01 +0200

    add-command-to-kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         commands\UpdateProbes::class,
+        commands\AddhistoryCore::class,
     ];
 
     /**
@@ -25,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('update:probes')->everyMinute();
+        $schedule->command('AddHistory:cores')->everyMinute();
     }
 
     /**
